32afc4e0b50c98fd49afd2b2857856116c8fdf44
LuckyLondo <ondoRox@gmail.com>
2020-07-09 11:19:34 -0600

    Initial Spring Initializr Checkin

---
+++ src/test/java/com/hiking/hikingapi/HikingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiking.hikingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HikingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
