dd685fbac86a863d6dbc144b9a2ea0b00d0dc719
LuckyLondo <ondoRox@gmail.com>
2020-07-09 15:21:42 -0600

    Added SpringDoc, StatusConroller, and Custom Error Handling

---
+++ src/main/java/com/hiking/hikingapi/config/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.hiking.hikingapi.config;
+
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler({ Exception.class })
+    public ResponseEntity<Object> handleAllExceptions(final Exception ex, final WebRequest request) {
+        logger.info(ex.getClass().getName());
+        logger.error("error", ex);
+        final ApiError apiError = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, ex.getLocalizedMessage(), "error occurred");
+        return new ResponseEntity<Object>(apiError, new HttpHeaders(), apiError.getStatus());
+    }
+}
---
+++ src/test/java/com/hiking/hikingapi/StatusControllerTests.java
@@ -0,0 +1,37 @@
+package com.hiking.hikingapi;
+
+import com.hiking.hikingapi.controllers.StatusController;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StatusControllerTests {
+
+    @Mock
+    private StatusController controller;
+    private String statusResult;
+
+    @Before
+    public void setup() {
+        controller = new StatusController();
+        statusResult = "{\"status\": \"Ok\"}";
+    }
+
+    @Test
+    public void testStatus() {
+        when(controller.status()).thenReturn(statusResult);
+        assertEquals(controller.status(), statusResult);
+    }
+
+
+
+}
