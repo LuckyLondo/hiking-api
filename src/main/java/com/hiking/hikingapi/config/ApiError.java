a713cd6bbbf519175a7fa1fe1d33b7e074901ff6
LuckyLondo <ondoRox@gmail.com>
2020-07-16 14:37:52 -0600

    Feature/luckylondo/custom error handling (#1)
    * Added SpringDoc, StatusConroller, and Custom Error Handling
    
    * Cleaned Up Test Code a Little
    
    * Fixed Test Case for Error
    
    * Added new lines to end of files

---
+++ src/main/java/com/hiking/hikingapi/config/ApiError.java
@@ -0,0 +1,33 @@
+package com.hiking.hikingapi.config;
+
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Data
+public class ApiError {
+
+    private HttpStatus status;
+    private String message;
+    private List<String> errors;
+    private String ryan;
+
+    public ApiError(HttpStatus status, String message, List<String> errors) {
+        super();
+        this.status = status;
+        this.message = message;
+        this.errors = errors;
+        this.ryan ="ryan";
+    }
+
+    public ApiError(HttpStatus status, String message, String error) {
+        super();
+        this.status = status;
+        this.message = message;
+        this.ryan = "ryan";
+        errors = Arrays.asList(error);
+    }
+
+}
---
+++ src/test/java/com/hiking/hikingapi/StatusControllerTests.java
@@ -0,0 +1,40 @@
+package com.hiking.hikingapi;
+
+import com.hiking.hikingapi.controllers.StatusController;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StatusControllerTests {
+
+    private StatusController controller;
+    private String statusResult;
+
+    @Before
+    public void setup() {
+        controller = new StatusController();
+        statusResult = "{\"status\": \"Ok\"}";
+    }
+
+    @Test
+    public void testStatus() {
+        assertEquals(controller.status(), statusResult);
+    }
+
+    @Test
+    public void testError() {
+        Exception ex = assertThrows(Exception.class, () -> { controller.error(); });
+        assertEquals(ex.getMessage(), "SHIT");
+    }
+
+
+
+}
